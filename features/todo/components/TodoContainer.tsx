4fcf0a4c2032f712a94a91521ec71a91dc45b27b
arifd <ademirkan@ucsd.edu>
2025-02-08 09:00:10 -0800

    feat: refactored todo component with react query custom hook

--- features/todo/components/TodoContainer.tsx
+++ features/todo/components/TodoContainer.tsx
@@ -1,158 +1,82 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 import CreateTodoItem from "./CreateTodoItem";
-import { Todo } from "@/types/todoTypes";
 import useAuthStore from "@/store/useAuthStore";
-import todoService from "@/service/todoService";
+import useTodos from "../hooks/useTodos";
 
 const TodoContainer: React.FC = () => {
     const { user, loading: authLoading } = useAuthStore();
-    const [todos, setTodos] = useState<Todo[]>([]);
+    const {
+        todos,
+        isLoading,
+        isError,
+        toggleTodo,
+        editTodo,
+        addTodo,
+        deleteTodo,
+    } = useTodos();
+
+    // Local state to control whether we're adding a new todo.
     const [isAdding, setIsAdding] = useState(false);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (user) {
-            todoService.fetchAllTodos().then((fetchedTodos) => {
-                setTodos(fetchedTodos);
-                setLoading(false);
-            });
-        } else if (!authLoading) {
-            setTodos([]);
-            setLoading(false);
-        }
-    }, [user, authLoading]);
-
-    const handleToggle = (id: string): void => {
-        // Get current todo status
-        const currentTodoStatus = todos.find((todo) => todo.id === id)
-            ?.completed as boolean;
-
-        // Optimistic update
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) =>
-                todo.id === id
-                    ? { ...todo, completed: !currentTodoStatus }
-                    : todo
-            )
-        );
-
-        // Update server
-        todoService
-            .updateTodoCompletion(id, !currentTodoStatus)
-            .catch((error: Error) => {
-                console.error(error);
-                // Rollback
-                setTodos((prevTodos) =>
-                    prevTodos.map((todo) =>
-                        todo.id === id
-                            ? { ...todo, completed: currentTodoStatus }
-                            : todo
-                    )
-                );
-            });
-    };
-
-    const handleEdit = (id: string, text: string): void => {
-        const currentTodo = todos.find((todo) => todo.id === id);
-        if (!currentTodo) {
-            return;
-        }
-
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
-        );
-
-        todoService.updateTodoText(id, text).catch((error: Error) => {
-            setTodos((prevTodos) =>
-                prevTodos.map((todo) => (todo.id === id ? currentTodo : todo))
-            );
-        });
-    };
-
-    const handleAddTodo = (text: string): void => {
-        if (!user) {
-            console.error("User is not authenticated.");
-            return;
-        }
-
-        setIsAdding(false);
-
-        // Optimistic update
-        const tempId = Date.now().toString();
-        setTodos((prevTodos) => [
-            ...prevTodos,
-            {
-                id: tempId,
-                text,
-                completed: false,
-                uid: user.uid,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-            },
-        ]);
-
-        // Add to server
-        todoService
-            .createTodo(text)
-            .then((todo) => {
-                setTodos((prevTodos) =>
-                    prevTodos.map((t) => (t.id === tempId ? todo : t))
-                );
-            })
-            .catch((error) => {
-                // Rollback
-                console.error(error);
-                setTodos((prevTodos) =>
-                    prevTodos.filter((todo) => todo.id !== tempId)
-                );
-            });
-    };
-
-    const handleDeleteTodo = (id: string): void => {
-        const currentTodo = todos.find((todo) => todo.id === id);
-        if (!currentTodo) {
-            return;
-        }
-        // Optimistic update
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-
-        // Delete from server
-        todoService.deleteTodo(id).catch((error) => {
-            // Rollback
-            setTodos((prevTodos) => [...prevTodos, currentTodo]);
-        });
-    };
-
-    if (authLoading || loading) {
+    // Display loading until both auth and todos data are ready.
+    if (authLoading) {
         return <div>Loading...</div>;
     }
+
+    // If not signed in, prompt the user to sign in.
+    if (!user) {
+        return <div>Please sign in to view your todos.</div>;
+    }
+
+    // Handler to toggle a todo's completed status.
+    const handleToggle = (id: string): void => {
+        const todo = todos.find((todo) => todo.id === id);
+        if (!todo) return;
+        toggleTodo(id, !todo.completed);
+    };
+
+    // Handler to update a todo's text.
+    const handleEdit = (id: string, text: string): void => {
+        const todo = todos.find((todo) => todo.id === id);
+        if (!todo) return;
+        editTodo(id, text);
+    };
+
+    // Handler to delete a todo.
+    const handleDeleteTodo = (id: string): void => {
+        const todo = todos.find((todo) => todo.id === id);
+        if (!todo) return;
+        deleteTodo(id);
+    };
+
+    // Handler to add a new todo.
+    const handleAddTodo = (text: string): void => {
+        setIsAdding(false);
+        addTodo(text);
+    };
 
     return (
         <div className="flex flex-col items-center justify-center">
             <h1>Todo List</h1>
-            {user ? (
-                <>
-                    <TodoList
-                        todos={todos}
-                        onToggle={handleToggle}
-                        onDelete={handleDeleteTodo}
-                        onEdit={handleEdit}
+            <>
+                <TodoList
+                    todos={todos}
+                    onToggle={handleToggle}
+                    onEdit={handleEdit}
+                    onDelete={handleDeleteTodo}
+                />
+                {isAdding ? (
+                    <CreateTodoItem
+                        onAdd={handleAddTodo}
+                        onCancel={() => setIsAdding(false)}
                     />
-                    {isAdding ? (
-                        <CreateTodoItem
-                            onAdd={handleAddTodo}
-                            onCancel={() => setIsAdding(false)}
-                        />
-                    ) : (
-                        <button onClick={() => setIsAdding(true)}>+</button>
-                    )}
-                </>
-            ) : (
-                <div>Please sign in to view your todos.</div>
-            )}
+                ) : (
+                    <button onClick={() => setIsAdding(true)}>+</button>
+                )}
+            </>
         </div>
     );
 };
---
+++ features/todo/hooks/useTodos.ts
@@ -0,0 +1,203 @@
+"use client";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "@/types/todoTypes";
+import todoService from "@/service/todoService";
+import useAuthStore from "@/store/useAuthStore";
+
+function useTodos() {
+    const { user, loading: authLoading } = useAuthStore();
+
+    const queryClient = useQueryClient();
+
+    // Fetch todos when a user is authenticated.
+    const { data, isLoading, error } = useQuery<Todo[]>({
+        queryKey: ["todos", user?.uid],
+        queryFn: () => todoService.fetchAllTodos(),
+        enabled: !!user?.uid,
+    });
+
+    const todos = data ?? [];
+
+    // Mutation to toggle a todo's completion status.
+    const statusMutation = useMutation({
+        mutationFn: ({ id, newStatus }: { id: string; newStatus: boolean }) =>
+            todoService.updateTodoCompletion(id, newStatus),
+        onMutate: ({ id, newStatus }) => {
+            // Retrieve the previous state.
+            const previousTodos = queryClient.getQueryData<Todo[]>([
+                "todos",
+                user?.uid,
+            ]);
+
+            // Optimistically update the completed status.
+            queryClient.setQueryData<Todo[]>(
+                ["todos", user?.uid],
+                (old: Todo[] | undefined) =>
+                    old?.map((todo) =>
+                        todo.id === id
+                            ? { ...todo, completed: newStatus }
+                            : todo
+                    ) ?? []
+            );
+            return { previousTodos };
+        },
+        onError: (err, variables, context) => {
+            if (context?.previousTodos) {
+                queryClient.setQueryData(
+                    ["todos", user?.uid],
+                    context.previousTodos
+                );
+            }
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos", user?.uid] });
+        },
+    });
+
+    // Mutation to update a todo's text.
+    const editTextMutation = useMutation({
+        mutationFn: ({ id, text }: { id: string; text: string }) =>
+            todoService.updateTodoText(id, text),
+        onMutate: ({ id, text }: { id: string; text: string }) => {
+            const previousTodos = queryClient.getQueryData<Todo[]>([
+                "todos",
+                user?.uid,
+            ]);
+
+            queryClient.setQueryData<Todo[]>(
+                ["todos", user?.uid],
+                (old: Todo[] | undefined) =>
+                    old?.map((todo) =>
+                        todo.id === id ? { ...todo, text } : todo
+                    ) ?? []
+            );
+            return { previousTodos };
+        },
+        onError: (
+            err: Error,
+            variables: { id: string; text: string },
+            context: any
+        ) => {
+            if (context?.previousTodos) {
+                queryClient.setQueryData(
+                    ["todos", user?.uid],
+                    context.previousTodos
+                );
+            }
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos", user?.uid] });
+        },
+    });
+
+    // Mutation to add a new todo.
+    const addMutation = useMutation({
+        mutationFn: (text: string) => todoService.createTodo(text),
+        onMutate: (text: string) => {
+            if (!user?.uid) {
+                throw new Error("User not authenticated");
+            }
+
+            const previousTodos = queryClient.getQueryData<Todo[]>([
+                "todos",
+                user?.uid,
+            ]);
+            const tempId = Date.now().toString();
+            const optimisticTodo: Todo = {
+                id: tempId,
+                text,
+                completed: false,
+                uid: user.uid,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            };
+
+            queryClient.setQueryData<Todo[]>(["todos", user?.uid], (old) =>
+                old ? [...old, optimisticTodo] : [optimisticTodo]
+            );
+            return { previousTodos, tempId };
+        },
+        onError: (err, text, context: any) => {
+            if (context?.previousTodos) {
+                queryClient.setQueryData(
+                    ["todos", user?.uid],
+                    context.previousTodos
+                );
+            }
+        },
+        onSuccess: (newTodo, text, context: any) => {
+            queryClient.setQueryData<Todo[]>(
+                ["todos", user?.uid],
+                (old) =>
+                    old?.map((todo) =>
+                        todo.id === context?.tempId ? newTodo : todo
+                    ) ?? []
+            );
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos", user?.uid] });
+        },
+    });
+
+    // Mutation to delete a todo.
+    const deleteMutation = useMutation({
+        mutationFn: (id: string) => todoService.deleteTodo(id),
+        onMutate: (id: string) => {
+            const previousTodos = queryClient.getQueryData<Todo[]>([
+                "todos",
+                user?.uid,
+            ]);
+            queryClient.setQueryData<Todo[]>(
+                ["todos", user?.uid],
+                (old: Todo[] | undefined) =>
+                    old?.filter((todo) => todo.id !== id) ?? []
+            );
+            return { previousTodos };
+        },
+        onError: (err, id, context) => {
+            if (context?.previousTodos) {
+                queryClient.setQueryData(
+                    ["todos", user?.uid],
+                    context.previousTodos
+                );
+            }
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: ["todos", user?.uid] });
+        },
+    });
+
+    // Utility functions to trigger mutations.
+    const toggleTodo = (id: string, newStatus: boolean) => {
+        statusMutation.mutate({ id, newStatus });
+    };
+
+    const editTodo = (id: string, text: string) => {
+        editTextMutation.mutate({ id, text });
+    };
+
+    const addTodo = (text: string) => {
+        addMutation.mutate(text);
+    };
+
+    const deleteTodo = (id: string) => {
+        deleteMutation.mutate(id);
+    };
+
+    return {
+        todos,
+        isLoading:
+            isLoading ||
+            addMutation.isPending ||
+            statusMutation.isPending ||
+            deleteMutation.isPending ||
+            editTextMutation.isPending,
+        isError: !!error,
+        toggleTodo,
+        editTodo,
+        addTodo,
+        deleteTodo,
+    };
+}
+
+export default useTodos;
